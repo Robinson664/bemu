b4f033ac397f74b879fceb421fb9515d99920750
unknown <jrobinson@WS-MAD-7Y6D8P1A.Forefront.local>
2013-03-06 15:24:36 -0500

    Put in explanations of the optional Intraday Tick Request settings.

--- Examples/IntradayTickDataRequest.cs
+++ Examples/IntradayTickDataRequest.cs
@@ -26,17 +26,38 @@ namespace Examples
 
             Service refDataService = session.GetService("//blp/refdata");
             Request request = refDataService.CreateRequest("IntradayTickRequest");
-            request.Set("security", "CMG US Equity");
-            request.Append("eventTypes", "TRADE"); //in TRADE (default), BID, ASK, BID_BEST, ASK_BEST, MID_PRICE, AT_TRADE, BEST_BID, BEST_ASK
+
+            request.Set("security", "SPY US Equity");
+            request.Append("eventTypes", "TRADE"); //One of TRADE (default), BID, ASK, BID_BEST, ASK_BEST, MID_PRICE, AT_TRADE, BEST_BID, BEST_ASK (see documentation A.2.6 for explanations)
             request.Set("startDateTime", new Datetime(DateTime.Today.AddHours(9.5)));
-            request.Set("endDateTime", new Datetime(DateTime.Today.AddHours(10).AddMinutes(1))); //goes back at most 140 days (documentation section 7.2.3)
-            request.Set("includeConditionCodes", true); //true or false (default)
-            request.Set("includeNonPlottableEvents", false); //true or false (default)
-            request.Set("includeExchangeCodes", false); //true or false (default)
-            request.Set("returnEids", false); //true or false (false)
-            request.Set("includeBrokerCodes", false); //true or false (default)
-            request.Set("includeRpsCodes", false); //true or false (default)
-            request.Set("includeBicMicCodes", false); //true or false (default)
+            request.Set("endDateTime", new Datetime(DateTime.Today.AddHours(9.8))); //goes back at most 140 days (documentation section 7.2.3)
+            
+            //A comma delimited list of exchange condition codes associated with the event. Review QR<GO> for more information on each code returned.
+            request.Set("includeConditionCodes", false); //Optional bool. Valid values are true and false (default = false)
+
+            //Returns all ticks, including those with condition codes.
+            request.Set("includeNonPlottableEvents", false); //Optional bool. Valid values are true and false (default = false)
+
+            //The exchange code where this tick originated. Review QR<GO> for more information.
+            request.Set("includeExchangeCodes", false); //Optional bool. Valid values are true and false (default = false)
+
+            //Option on whether to return EIDs for the security.
+            request.Set("returnEids", false); //Optional bool. Valid values are true and false (default = false)
+
+            //The broker code for Canadian, Finnish, Mexican, Philippine, and Swedish equities only.
+            //  The Market Maker Lookup screen, MMTK<GO>, displays further information on market makers and their corresponding codes.
+            request.Set("includeBrokerCodes", false); //Optional bool. Valid values are true and false (default = false)
+
+            //The Reporting Party Side. The following values appear:
+            //  -B: A customer transaction where the dealer purchases securities from the customer.
+            //  -S: A customer transaction where the dealer sells securities to the customer.
+            //  -D: An inter-dealer transaction (always from the sell side).
+            request.Set("includeRpsCodes", false); //Optional bool. Valid values are true and false (default = false)
+
+            //The BIC, or Bank Identifier Code, as a 4-character unique identifier for each bank that executed and reported the OTC trade, as required by MiFID.
+            //  BICs are assigned and maintained by SWIFT (Society for Worldwide Interbank Financial Telecommunication).
+            //  The MIC is the Market Identifier Code, and this indicates the venue on which the trade was executed.
+            request.Set("includeBicMicCodes", false); //Optional bool. Valid values are true and false (default = false)
 
             CorrelationID corr = new CorrelationID(17);
 
